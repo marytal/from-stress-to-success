8c92001f73893b01e93afb1d8de9d5dbfd096ab0
Jan Hellmich <jan@futureadvisor.com>
2016-02-24 11:31:19 -0800

    Initial Commit

---
+++ src/Header.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+export class Header extends Component {
+  render() {
+    return (
+      <div>FROM STRESS TO SUCCESS! This is the Header View</div>
+    );
+  }
+}
