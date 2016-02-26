98b4a183f3b6db301936f4203567d3004dd0103b
Mary Briskin <mary.briskin@futureadvisor.com>
2016-02-26 09:01:12 -0800

    Configures chart to have commas for thousand separators

--- src/Chart.js
+++ src/Chart.js
@@ -9,8 +9,7 @@ var retirementAge = 60;
 export default class Chart extends Component {
   render() {
     let {chartData, scenarioIndex} = this.props;
-    console.log(scenarioIndex)
-    let chart = Object.keys(this.props.chartData).length > 0 ? <ReactHighcharts config = {generateChartConfig(chartData, age, 57, retirementAge, scenarioIndex)}></ReactHighcharts> : null
+    let chart = Object.keys(this.props.chartData).length > 0 ? <ReactHighcharts config = {generateChartConfig(chartData, age, 40, retirementAge, scenarioIndex)}></ReactHighcharts> : null
     return (
       <div>
         {chart}
